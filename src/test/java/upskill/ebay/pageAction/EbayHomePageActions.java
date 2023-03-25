176adb3b62c28f6ddcc57ac12c17ed5f1b5dad92
iNoobkiller <Intishar70@gmail.com>
2023-03-25 16:27:44 -0400

    4th commit

--- src/test/java/upskill/ebay/pageAction/EbayHomePageActions.java
+++ src/test/java/upskill/ebay/pageAction/EbayHomePageActions.java
@@ -1,10 +1,15 @@
 package upskill.ebay.pageAction;
 
 import java.security.Key;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import upskill.ebay.pageElements.EbayHomePageLocators;
 import upskill.utilities.SetupDrivers;
@@ -41,9 +46,9 @@ public class EbayHomePageActions {
 	}
 	
 	public void clickSummary() throws Exception {
-	EbayHomePageLocatorsObj.linkSummary.isEnabled();
-	EbayHomePageLocatorsObj.linkSummary.click();
-	Thread.sleep(5000);
+		EbayHomePageLocatorsObj.linkSummary.isEnabled();
+		EbayHomePageLocatorsObj.linkSummary.click();
+		Thread.sleep(5000);
 	}
 	public void KeyboardKey() {
 		EbayHomePageLocatorsObj.btnSearch.sendKeys(Keys.RETURN);
@@ -53,4 +58,61 @@ public class EbayHomePageActions {
 		EbayHomePageLocatorsObj.btnSearch.sendKeys(Keys.DELETE);
 		
 	}
-}
+	public void handleAlert() {
+		SetupDrivers.driver.switchTo().alert().dismiss();
+		SetupDrivers.driver.switchTo().alert().dismiss();
+		SetupDrivers.driver.switchTo().alert().getText();
+		SetupDrivers.driver.switchTo().alert().sendKeys("awesom");
+	}
+	public void synchronization(){
+		/*	Selenium Wait : 
+				1. Implicit wait(Global), 
+				2. Explicit wait(Conditional), 
+				3. Fluent wait(intermittent) */
+			
+		//Implicit Wait
+		SetupDrivers.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		//Explicit Wait
+		WebDriverWait explicitWait = new WebDriverWait(SetupDrivers.driver, 10);
+		explicitWait.until(ExpectedConditions.elementToBeClickable(EbayHomePageLocatorsObj.btnSearch));
+		
+		//Fluent Wait
+				FluentWait fluentWait = new FluentWait(SetupDrivers.driver);								
+				fluentWait.withTimeout(10, TimeUnit.SECONDS);
+				fluentWait.pollingEvery(2, TimeUnit.SECONDS);
+	}
+	public void javaScriptExecutor(){
+		
+		JavascriptExecutor js = (JavascriptExecutor)SetupDrivers.driver;	//Creating JS object
+		
+		js.executeScript("");
+		
+		js.executeScript("EbaySearchResultLocatorsObj.cbxBrandNike.click();"); //Clicking on element
+		
+		js.executeScript("EbayHomePageLocatorsObj.txtbxSearch.value ='shirt';"); //Writing something
+		
+		js.executeScript("EbaySearchResultLocatorsObj.cbxBrandNike.checked=true;"); //Interect Checkbox
+		
+		js.executeScript("window.location = 'http://upskill.com';"); // initializing location
+		
+		js.executeScript("location.reload()"); 						//Refresh browser
+		
+		js.executeScript("alert('Confirmation');");					//Alert
+		
+		js.executeScript("window.scrollBy(0,500)", ""); 			//Scroll down to specific pixel
+		
+		js.executeScript("window.scrollBy(0,-500)", ""); 			//Scroll up to specific pixel
+		
+		js.executeScript("arguments[0].scrollIntoView();", EbayHomePageLocatorsObj.btnSearch);  //Scroll to a object
+		
+		js.executeScript("window.scrollBy(0,document.body.scrollHeight)"); //Scroll down to bottom of website
+	}
+	}
+	
+	
+
+
+
+
+
